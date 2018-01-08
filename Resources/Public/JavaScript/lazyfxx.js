23b07e30fe9952ebc345ebf28f80acd693ae509d
guelzow <tobias.guelzow@brainworxx.de>
2018-01-08 09:55:33 +0100

    Updated the copyright year.

--- Resources/Public/JavaScript/lazyfxx.js
+++ Resources/Public/JavaScript/lazyfxx.js
@@ -11,7 +11,7 @@
  *
  *   GNU Lesser General Public License Version 2.1
  *
- *   lazyFxx Copyright (C) 2017 Brainworxx GmbH
+ *   lazyFxx Copyright (C) 2017-2018 Brainworxx GmbH
  *
  *   This library is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU Lesser General Public License as published by
